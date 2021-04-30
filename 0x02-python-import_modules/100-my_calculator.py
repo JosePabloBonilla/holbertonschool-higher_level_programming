dfbaca551cae3b5ba3e1d29d647ae441a8638e59
JosePabloBonilla <2750@holbertonschool.com>
2021-04-30 02:11:45 +0000

    fix file 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,5 +18,5 @@ if __name__ == '__main__':
         print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
     elif sys.argv[2] == '*':
         print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
+    else:
         print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
