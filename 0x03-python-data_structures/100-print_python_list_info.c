c97bd05296bec71700adbfdae4ef4ba141df41ff
JosePabloBonilla <2750@holbertonschool.com>
2021-05-04 22:31:13 +0000

    add file 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: object
+ * Return: nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+	(void)*p;
+}
