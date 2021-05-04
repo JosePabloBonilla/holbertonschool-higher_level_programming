2e66407927088b5b4af87a39c6168bfd68a68f5f
JosePabloBonilla <2750@holbertonschool.com>
2021-05-04 22:20:18 +0000

    add lists.h file 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * is_palindrome - check if list is a palindrome (mirror)
+ * @head: head of list
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	(void)*head;
+	return (0);
+}
