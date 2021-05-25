7384883508754d6c7d2dd4639704268547b61bd5
JosePabloBonilla <2750@holbertonschool.com>
2021-05-25 02:03:17 +0000

    [fix] file 2 comments

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,43 +6,45 @@ Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)
 
 class Rectangle():
     """Defines a rectangle"""
-
     def __init__(self, width=0, height=0):
+        """initialize values width and height"""
         self.width = width
         self.height = height
 
-    """width"""
     @property
     def width(self):
+        """width"""
         return (self.width)
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """height"""
     @property
     def height(self):
+        """height"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    """rectangle area"""
     def area(self):
+        """rectangle area"""
         return (self.__height * self.__height)
 
-    """rectangle perimeter"""
     def perimeter(self):
+        """rectangle perimeter"""
         if self.width == 0:
             return (0)
         if self.height == 0:
