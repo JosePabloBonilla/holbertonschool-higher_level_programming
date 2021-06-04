7eb073290aaec2c75b9cacdfff095b0b52281041
JosePabloBonilla <2750@holbertonschool.com>
2021-06-04 01:41:15 +0000

    [fix] file 1 pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@ writes a string to a file
 """
 
 
-def write_file(filename="", text =""):
+def write_file(filename="", text=""):
     """writes string to a file"""
     with open(filename, 'w', encoding='utf-8') as fd:
         return (fd.write(text))
