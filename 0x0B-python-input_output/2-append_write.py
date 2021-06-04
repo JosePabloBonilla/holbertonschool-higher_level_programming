b2cf2ee50c81f0c93e50f8978ed637be0f66d46f
JosePabloBonilla <2750@holbertonschool.com>
2021-06-04 02:00:55 +0000

    [fix] file 2 pep8

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,6 +4,7 @@ write a function that appends a string at the end of a text file (UTF8)
 and returns the number of characters added
 """
 
+
 def append_write(filename="", text=""):
     """appends string at the end of a text file"""
     with open(filename, 'a', encoding="utf-8") as fd:
