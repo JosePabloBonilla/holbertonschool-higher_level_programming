9a4f4a402250e7f961630f4e75bf5f6ea367fc82
JosePabloBonilla <2750@holbertonschool.com>
2021-08-13 18:58:56 +0000

    [fix] file 9 pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+Write a script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa
 """
 from sys import argv
 from sqlalchemy import create_engine
