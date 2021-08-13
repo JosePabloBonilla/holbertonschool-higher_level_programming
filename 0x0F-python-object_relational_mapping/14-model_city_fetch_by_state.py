3b878b79987f49cad5c97bc2a1a814885db211f8
JosePabloBonilla <2750@holbertonschool.com>
2021-08-13 17:29:47 +0000

    [add] files 6-14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a Python file similar to model_state.py
+named model_city.py that contains the class definition of a City.
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]))
+    InstanceSession = sessionmaker(bind=engine)
+    session = InstanceSession()
+
+    for state, city in session.query(State, City).join(
+            City).order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
