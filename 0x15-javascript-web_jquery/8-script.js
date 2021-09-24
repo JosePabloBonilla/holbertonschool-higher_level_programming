249c65c3817962254af68ac24e4781bbc0c1746e
JosePabloBonilla <2750@holbertonschool.com>
2021-09-24 01:36:43 +0000

    [add] all files

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json',
+function (data) {
+	for (const i of data.results) {
+		$('UL#list_movies').append('<li>' + i.title + '</li>');
+	}	
+});
