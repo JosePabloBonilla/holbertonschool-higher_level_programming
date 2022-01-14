3c17111d12f4d557edfcf020c588bd527f1caeae
Jose Pablo Bonilla <josepablo938@gmail.com>
2022-01-14 02:42:50 +0000

    fix file 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """takes email, sends a POST request to the passed URL"""
-import urllib.request
-import urllib.parse
+import urllib.request import Request, urlopen
+import urllib.parse import urlencode
 import sys
 
 
 if __name__ == "__main__":
 
-    url = sys.argv[1]
     values = {'email': sys.argv[2]}
+    data = urlencode(values).encode('utf-8')
+    response = Request(argv[1], data)
 
-    data = urllib.parse.urlencode(values)
-    data = urlencode(url).encode('utf-8')
-    req = Request(argv[1], data)
-
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-    print(the_page.decode('utf-8'))
+    with urlopen(response) as File:
+        print(File.read().decode('utf-8'))
